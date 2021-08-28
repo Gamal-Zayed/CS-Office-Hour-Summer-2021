ce1d86b671fe80a24d975e4cc5ccf3af3245e036
Gamal Zayed <78564100+Gamal-Zayed@users.noreply.github.com>
2021-08-28 16:39:44 +0200

    Update and rename Services-Project to Services-Project.py

--- Services-Project.py
+++ Services-Project.py
@@ -1,7 +1,18 @@
+bill = 0
 x = int(input('please insert the number of service you want:\nfor ..... press 1\n for ... press 2\n for .... press 3'))
 if x==1:
   price = data['Jump_Starter']
 elif x==2:
   price = data['....']
+  .
+  .
+  .
   
+ bill = bill + price
+
+acc_price = 0
+y = int(input('plz insert accessories:\n for....))
+if y==1:
+              dkcndkvndkn
+bill = bill + acc_price
   
