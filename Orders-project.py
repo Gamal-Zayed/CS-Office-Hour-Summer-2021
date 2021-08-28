02a331380a6dab7bf44381dbf47cea248b7be59d
Gamal Zayed <78564100+Gamal-Zayed@users.noreply.github.com>
2021-08-28 17:20:22 +0200

    Update Orders-project.py

--- Orders-project.py
+++ Orders-project.py
@@ -1,6 +1,24 @@
-list1 = [lablabla]
-list2 = [habal]
+my_functions.py
+def main():
+  list1 = [lablabla]
+  list2 = [habal]
 
-for i in range(len(list1):
-               ......list1[i]......
-               ......list2[i].......
+  for i in range(len(list1):
+                 ......list1[i]......
+                 ......list2[i].......
+return cost, order
+                 
+                 
+               
+ #in main file:
+ import my_functions           
+ #customers_data = []
+ n = int(input(('how many'))
+ for i in range(n):
+  dict = {}
+  dict['cost'],dict['order'] = my_functions.main()
+  #customers_data.append(dict)   
+ 
+ 
+         
+               
